d81a00f41623a5d388db546bbef84fc2bd82a25f
laiy <ly.franky@gmail.com>
2014-11-17 21:47:15 +0800

    somebody keeps erasing my changes.

---
+++ hw3/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#	> File Name: main.py
+#	> Author: LY
+#	> Mail: ly.franky@gmail.com
+#	> Created Time: Monday, November 17, 2014 PM07:54:24 CST
+
+import os.path
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from models import ReviewsModule, IndexHandler
+from tornado.options import define, options
+
+define("port", default=2333, help="run on the given port", type=int)
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r'/', IndexHandler)], template_path=os.path.join(os.path.dirname(__file__), "tpl"),
+        static_path=os.path.join(os.path.dirname(__file__), "assets"), ui_modules={'Reviews': ReviewsModule}, debug=True)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
---
+++ hw3/models.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#	> File Name: moveInfo.py
+#	> Author: LY
+#	> Mail: ly.franky@gmail.com
+#	> Created Time: Monday, November 17, 2014 PM07:59:52 CST
+
+import os.path
+import glob
+import tornado.web
+
+class MovieInfo(object):
+    def __init__(self, filePath):
+        movieFile = open(os.path.join(filePath, 'info.txt'))
+        (self.title, self.year, self.rating, self.total) = movieFile.read().splitlines()
+        self.dir = filePath.split(os.sep)[-1]
+
+class ReviewMsg(object):
+    def __init__(self, quote, rating, reviewer, company):
+        self.quote = quote
+        self.rating = rating
+        self.reviewer = reviewer
+        self.company = company
+
+class Reviews(object):
+    def __init__(self, filePath):
+        self.left = []
+        self.right = []
+        reviews = glob.glob(os.path.join(filePath, 'review[0-9]*.txt'))
+        self._len = len(reviews)
+        reviews.sort()
+        for i in range(self._len):
+            file = open(reviews[i])
+            (quote, rating, reviewer, company) = file.read().splitlines()
+            msg = ReviewMsg(quote, rating, reviewer, company)
+            self.left.append(msg) if i < self._len / 2 else self.right.append(msg)
+
+class ReviewsModule(tornado.web.UIModule):
+    def render(self, reviews):
+        return self.render_string('includes/review.html', reviews=reviews)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        film = self.get_argument('film', 'tmnt')
+        filePath = os.path.join(os.path.dirname(__file__), 'assets', 'movies', film)
+        movieInfo = MovieInfo(filePath)
+        overviews = open(os.path.join(filePath, 'generaloverview.txt'))
+        overview = list()
+        for line in overviews:
+            line = line.strip()
+            (term, sep, data) = line.partition(':')
+            overview.append((term, data))
+        reviews = Reviews(filePath)
+        self.render('movie.html', movieInfo=movieInfo, overview=overview, reviews=reviews)
+
